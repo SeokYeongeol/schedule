1595307d75e498229dcfaa6412d1ee764a1a82b5
cheoni5417 <cheoni5417@naver.com>
2025-02-02 20:54:37 +0900

    refactor : RequestMapping에서 주소를 Restful하게 변경

--- src/main/java/org/example/schedule/controller/ScheduleController.java
+++ src/main/java/org/example/schedule/controller/ScheduleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/schedule")
+@RequestMapping("/schedules")
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
