f61e9917bfcee17e046ee68a0f7eec1ef45b45c3
cheoni5417 <cheoni5417@naver.com>
2025-02-04 14:30:02 +0900

    refactor : 필요없는 부분 제거

--- src/main/java/org/example/schedule/entity/Schedule.java
+++ src/main/java/org/example/schedule/entity/Schedule.java
@@ -1,12 +1,10 @@
 package org.example.schedule.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 public class Schedule {
     private Long id;
     private String name;
