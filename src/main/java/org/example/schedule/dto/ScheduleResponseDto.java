ab840e967c9a6e1cd18144d4bdbdae12e4e20153
cheoni5417 <cheoni5417@naver.com>
2025-02-04 14:29:30 +0900

    refactor : 필요없는 부분 제거

--- src/main/java/org/example/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/org/example/schedule/dto/ScheduleResponseDto.java
@@ -1,19 +1,14 @@
 package org.example.schedule.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.schedule.entity.Schedule;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Getter
-@AllArgsConstructor
 public class ScheduleResponseDto {
     private Long id;
     private String name;
-    private String password;
     private String title;
     private String contents;
     private LocalDateTime createdAt;
